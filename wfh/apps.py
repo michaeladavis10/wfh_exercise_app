52847a21e4dd961b2da8967cf145903f75294ff2
Michael A Davis <mdavis@mdavis-air.local>
2020-04-12 21:30:45 -0700

    removing django

--- wfh/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WfhConfig(AppConfig):
-    name = 'wfh'
--- wfh/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from wfh import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("wfh/<name>", views.hello_there, name="hello_there"),
-    path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact"),
-]
